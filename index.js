9a3d32f07457e8236298c6e783d57f3e4aaa00f5
Dominique <deejay.whitegrass@gmail.com>
2016-01-27 11:27:34 -0600

    save

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ app.listen(port, function() {
   console.log('listening on port: ', port);
 });
 
-// app.get('/', function(req, res) {
-// 	res.sendfile('/index.html');
-// })
+app.get('/', function(req, res) {
+	res.sendFile('index.html');
+})
 
 // app.listen(app.get('port'), function() {
 //   console.log('Node app is running on port', app.get('port'));
