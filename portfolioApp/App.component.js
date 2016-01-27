d8f755a110ac80cff304622e519dec60d3834499
dominiquejane <deejay.whitegrass@gmail.com>
2016-01-27 05:40:45 +0000

    merge

--- portfolioApp/App.component.js
+++ portfolioApp/App.component.js
@@ -1,1 +1,26 @@
-App.component.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+var App = React.createClass({
+     
+     
+     render() {
+          return (
+               <div>
+               
+               </div>
+          )
+     }
+})
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
