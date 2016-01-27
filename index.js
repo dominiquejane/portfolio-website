0becd418d24e1b6a72cec7f8bd615a44668a7199
Dominique <deejay.whitegrass@gmail.com>
2016-01-27 11:25:35 -0600

    save

--- index.js
+++ index.js
@@ -2,24 +2,24 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var cors = require('cors');
-// var port = process.env.PORT || 9001;
+var port = process.env.PORT || 9001;
 var request = require('request');
 
-app.set('port', (process.env.PORT || port));
+// app.set('port', (process.env.PORT || port));
 
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.static(__dirname + '/public'));
 
-// app.listen(port, function() {
-//   console.log('listening on port: ', port);
-// });
+app.listen(port, function() {
+  console.log('listening on port: ', port);
+});
 
-app.get('/', function(req, res) {
-	res.sendfile('/index.html');
-})
+// app.get('/', function(req, res) {
+// 	res.sendfile('/index.html');
+// })
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-}); 
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// }); 
